724d3f30f30578ff8faa53e5af0e516b985ee0c2
Jetlonglim <133329143+Jetlonglim@users.noreply.github.com>
2025-02-10 14:58:49 +0800

    Add files via upload

--- HawkerLicensingSystem/system/static/script.js
+++ HawkerLicensingSystem/system/static/script.js
@@ -47,3 +47,29 @@ function register(event, form) {
     })
     .catch(error => console.error("Error:", error));
 }   
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
